f2b31dc7bd2fe509ae1261d72be73e3b8d6e8a74
吴雨楠 <wuyunan@wuyunandemacbook-air.local>
2016-07-22 13:28:25 -0500

    Storyboard Done

---
+++ MyOwnApp/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  MyOwnApp
+//
+//  Created by 吴雨楠 on 16/7/22.
+//  Copyright © 2016年 Yunan Wu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- MyOwnApp/ViewController.swift
+++ MyOwnApp/ViewController.swift
@@ -1,25 +1,15 @@
-//
-//  ViewController.swift
-//  MyOwnApp
-//
-//  Created by 吴雨楠 on 16/7/22.
-//  Copyright © 2016年 Yunan Wu. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
